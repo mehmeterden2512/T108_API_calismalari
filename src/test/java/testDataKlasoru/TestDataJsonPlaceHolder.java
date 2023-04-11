2869896bd4f87e6391c7c460b2f34dd519befc6d
Mehmet Erden <merden2512@gmail.com>
2023-04-11 13:54:50 +0300

    ders sonu

---
+++ src/test/java/testDataKlasoru/TestDataJsonPlaceHolder.java
@@ -0,0 +1,25 @@
+package testDataKlasoru;
+
+import org.json.JSONObject;
+
+public class TestDataJsonPlaceHolder {
+   public int basariliStatusCode=200;
+
+    public JSONObject expectedBodyOlusturJsonObject(){
+         /*
+         {
+    "userId":3,
+    "id":22,
+    "title":"dolor sint quo a velit explicabo quia nam",
+    "body":"eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita ear
+    um mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+    }
+         */
+        JSONObject expBody=new JSONObject();
+        expBody.put("userId",3);
+        expBody.put("id",22);
+        expBody.put("title","dolor sint quo a velit explicabo quia nam");
+        expBody.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+        return expBody;
+    }
+}
